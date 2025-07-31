6fa8d17838bb6a3f86b7d39b50381b34d0692cd1
kimshyuno3 <kimgrace030103@naver.com>
2025-07-31 21:34:03 +0900

    5번 스케줄 확정시 로직 문제 해결

--- albamate_sample/lib/screen/groupPage/schedule/boss_schdule_home.dart
+++ albamate_sample/lib/screen/groupPage/schedule/boss_schdule_home.dart
@@ -4,18 +4,32 @@ import 'package:albamate_sample/screen/groupPage/schedule/schedule_confirm_nav.d
 import 'package:albamate_sample/screen/groupPage/schedule/schedule_request_nav.dart';
 
 class BossScheduleHomePage extends StatefulWidget {
-  final String groupId; // ✅ 그룹 ID를 필수로 받음
+  /// 그룹 ID는 필수, initialIndex는 선택(기본값 0)
+  final String groupId;
+  final int initialIndex;
 
-  const BossScheduleHomePage({super.key, required this.groupId});
+  const BossScheduleHomePage({
+    super.key,
+    required this.groupId,
+    this.initialIndex = 0,
+  });
 
   @override
   State<BossScheduleHomePage> createState() => _BossScheduleHomePageState();
 }
 
 class _BossScheduleHomePageState extends State<BossScheduleHomePage> {
-  int selectedIndex = 0;
+  late int selectedIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    // 부모에서 전달받은 initialIndex를 사용해 탭 초기값 설정
+    selectedIndex = widget.initialIndex;
+  }
 
   void _onTabSelected(int index) {
+
     setState(() {
       selectedIndex = index;
     });
@@ -28,7 +42,12 @@ class _BossScheduleHomePageState extends State<BossScheduleHomePage> {
         groupId: widget.groupId,
         // TODO: ⚠️ 현재 userRole 임시 사용 중 (백엔드 ownerId 연동 시 제거 예정)
         userRole: '사장님',
-      ), // ✅ 그룹 ID 전달
+         onScheduleConfirmed: () {
+           setState(() {
+             selectedIndex = 1; // “스케줄 확정” 탭으로 전환
+           });
+         },
+      ),
       ScheduleConfirmNav(groupId: widget.groupId),
     ];
 
